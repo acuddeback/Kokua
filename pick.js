a1f1a2687f4599b211283b99872e2764eb1a411b
Cindy Zhang <xzhang17@belmontschools.net>
2016-08-03 09:59:31 -0400

    appending the list works

--- pick.js
+++ pick.js
@@ -1,11 +1,8 @@
-function pick() {
-	var names = [
-	'Zoe', 
-	'Christina', 
-	'Jessica', 
-	'Cindy', 
-	'Anna'
+var names = [
+	
 	]; 
+
+function pick() {
 	var picked = names[Math.round(Math.random()*(names.length-1))];
 	document.getElementById('call').innerHTML = picked
 }   				       	 //picks element from array
@@ -16,10 +13,10 @@ function save_data(){
 	console.log("Got name" + name + " from form");
 	localStorage.setItem('test-name', name);
 	names.push(name);               //append list here:
-
+	var container = document.getElementById('loaded_data');
+	container.innerHTML = localStorage.getItem("names");
 }
 
 function load_data(){
-	var container = document.getElementById('loaded_data');
-	container.innerHTML = localStorage.getItem('test-name');
+	
 }
