ec27368a06ce88cb82e5244471197164e11f1942
zoe402 <zweiss402@gmail.com>
2016-08-04 14:28:51 -0400

    there are pop ups and deletes and instructions and more!

--- coldcallerfunctions.js
+++ coldcallerfunctions.js
@@ -2,13 +2,15 @@ var names = [
 
 	]; 
 
+//used for cold caller
 function pick() {
 	
 	var picked = names[Math.round(Math.random()*(names.length-1))];
 	document.getElementById('call').innerHTML = picked
+	
 	//picks element from array
 }   				       	 
-//adds item to list in descending order
+/*NOT USED, JUST KEEPING THIS AS A JUST IN CASE
 function add_names(){
 	
 	var form = document.getElementById("form1");  								   //receives input
@@ -19,18 +21,15 @@ function add_names(){
 	var container = document.getElementById('loaded_data');
 	container.innerHTML += localStorage.getItem("class") + "<br>"; //stop it from printing whole list
 	
-}
+}*/
 
 //deletes the last item from the list in ascending order
 function deleteFromList(){
-	
-	var form = document.getElementById("form1");  								   //receives input
-	var name = form.elements[0].value;
-	console.log("Got name" + name + " from form");
-	names.pop(names);               //removes name from list here:
+	names.pop(name); //removes name from list here
 	localStorage.setItem("class", names);
 	var container = document.getElementById('loaded_data');
 	container.innerHTML = localStorage.getItem("class");
+	
 	var text = ""; //creates variable that will become what the code actually prints out
 	for(i = 0; i < names.length; i++) { //iterates through name list to do breaks
 		text += names[i] + "<br>"; //adds the break after every word
@@ -39,9 +38,12 @@ function deleteFromList(){
 	
 }
 
-
-
-
-/* <form id="form2">
-			<input type="checkbox" name= "names"> name
-		</form> */
+//creates pop up that asks for student's names
+function popup() {
+	var input = prompt("Student name:"); //creates pop up box
+	var name = input;
+	names.push(name); 	//append list here:
+	localStorage.setItem("class",name); //storing
+	var container = document.getElementById('loaded_data');
+	container.innerHTML += localStorage.getItem("class") + "<br>"; //stop it from printing whole list
+}
