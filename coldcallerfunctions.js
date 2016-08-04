711528112db1147dc0ac281f47abf5b16deedb32
zoe402 <zweiss402@gmail.com>
2016-08-04 11:13:40 -0400

    DELETE BUTTON WORKS!!!! <3<3:):):)

--- coldcallerfunctions.js
+++ coldcallerfunctions.js
@@ -8,7 +8,7 @@ function pick() {
 	document.getElementById('call').innerHTML = picked
 	//picks element from array
 }   				       	 
-
+//adds item to list in descending order
 function add_names(){
 	
 	var form = document.getElementById("form1");  								   //receives input
@@ -21,6 +21,7 @@ function add_names(){
 	
 }
 
+//deletes the last item from the list in ascending order
 function deleteFromList(){
 	
 	var form = document.getElementById("form1");  								   //receives input
@@ -30,7 +31,11 @@ function deleteFromList(){
 	localStorage.setItem("class", names);
 	var container = document.getElementById('loaded_data');
 	container.innerHTML = localStorage.getItem("class");
-	
+	var text = ""; //creates variable that will become what the code actually prints out
+	for(i = 0; i < names.length; i++) { //iterates through name list to do breaks
+		text += names[i] + "<br>"; //adds the break after every word
+		container.innerHTML = text; //container.innerHTML gets the code from inside and displays it on the screen
+	}
 	
 }
 
