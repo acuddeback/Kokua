8bcf95860e22e63b64dfa54de2f03f6d708b754d
annacuddeback <atc@cuddeback.org>
2016-08-05 16:00:55 -0400

    NEW COOL THINGS! 'groupmakeranna.js' and 'coldcallerpagewithgroupsanna.html' are the new files that kinda sorta work

--- groupgen.js
+++ groupgen.js
@@ -3,14 +3,6 @@ import Math
 //creates empty student list
 var studentlist = [];
 
-//asks for number of students
-var studentnum = int(input("How many students are in your class?"));
-
-//asks for student names 
-for i (i = 0; i < studentnum; i++){
-	studentlist.push(input("Add a student name: "));
-}
-
 //chooses a random index
 function getRandomIndex(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -25,7 +17,7 @@ function groupnumber(index){
 
 //defines function to generate groups
 function groupgenerator(){
-	var studentgenlist = studentlist; 
+	var studentgenlist = $studentlist; 
 	var maxstudent = int(input("What's the largest number of students you want in each group?"));
 	var groupnum = Math.ceil((len(studentgenlist)/maxstudent));
 
@@ -33,26 +25,28 @@ function groupgenerator(){
 	var grouplist = [];
 
 //creates individual group list
-	for i (i = 0; i < groupnum; i++){
+	for(i = 0; i < groupnum; i++){
 		var group = [] ;
 		//adds individual group to master group list 
 		grouplist.push(group);
 	}
 
 //adds students to individual lists
-	for i (i=0; i < len(studentgenlist); i++){
+	for(i=0; i < len(studentgenlist); i++){
 		var listindex = getRandomIndex(0, len(studentgenlist)-1);
 		var groupindex = (i % groupnum);
-		grouplist[groupindex].push(studentlist[listindex]);
-		del(studentlist[listindex]);
+		grouplist[groupindex].push($studentlist[listindex]);
+		del($studentlist[listindex]);
 	}
+var printlist = [];
 
 //prints lists in a comprehensive format
-	for i (i=0; i < len(grouplist); i++){	
-		print(groupnumber((i)));
+	for(i=0; i < len(grouplist); i++){	
+		printlist.push(groupnumber((i)));
 		var index = i; 
 	}
-		for i in range(i=0; i < len(grouplist[index]); i++){
-			print(grouplist[index][i]);
+		for(i=0; i < len(grouplist[index]); i++){
+			printlist.push(grouplist[index][i]);
 		}
+	document.getElementById("grouplists").innerHTML = "printlist"
 }		
