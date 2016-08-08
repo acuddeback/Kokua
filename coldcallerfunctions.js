f81ef6a2f0236b7e546a98b75f60c1a5458cd4f7
zoe402 <zweiss402@gmail.com>
2016-08-08 09:25:32 -0400

    working cold caller functions

--- coldcallerfunctions.js
+++ coldcallerfunctions.js
@@ -38,6 +38,20 @@ function deleteFromList(){
 	
 }
 
+//deletes last called name
+function uncalled(){
+	var index=names.indexOf(picked);
+	names.splice(index, 1); //removes name from list here:
+	localStorage.setItem("class", names);
+	var container = document.getElementById('loaded_data');
+	container.innerHTML = localStorage.getItem("class");
+	var text = ""; //creates variable that will become what the code actually prints out
+	for(i = 0; i < names.length; i++) { //iterates through name list to do breaks
+		text += names[i] + "<br>"; //adds the break after every word
+		container.innerHTML = text; //container.innerHTML gets the code from inside and displays it on the screen
+	}
+}
+
 //creates pop up that asks for student's names
 function popup() {
 	var input = prompt("Student name:"); //creates pop up box
